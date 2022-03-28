4525abce58d65a9b942f719ae1499565ee39085c
Arthur <arthurkaplan@gmx.at>
2022-03-28 20:33:45 +0200

    Changed views.py to accept html render

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': "C'est moi",
+        'title': "Blog Post 1",
+        'content': 'First post content',
+        'date_posted': 'March 28, 2022'
+    },
+    {
+        'author': "Still me",
+        'title': "Blog Post 2",
+        'content': 'Second post content',
+        'date_posted': 'March 29, 2022'
+    },
+    {
+        'author': "It's a me",
+        'title': "Blog Post 3",
+        'content': 'Third post content',
+        'date_posted': 'March 32, 2022'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request=request, template_name='blog/home.html', context=context)
 
 
 def about(request):
-    return HttpResponse('<h1>About this blog</h1>')
+    return render(request=request, template_name='blog/about.html', context={'title': 'About'})
