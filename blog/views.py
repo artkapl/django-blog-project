c4bed6d42d85a2f4cb72bac3c32fffcac7307573
Arthur <arthurkaplan@gmx.at>
2022-03-30 20:21:06 +0200

    Removed dummy data posts list and inserted Post DB table data in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': "C'est moi",
-        'title': "Blog Post 1",
-        'content': 'First post content',
-        'date_posted': 'March 28, 2022'
-    },
-    {
-        'author': "Still me",
-        'title': "Blog Post 2",
-        'content': 'Second post content',
-        'date_posted': 'March 29, 2022'
-    },
-    {
-        'author': "It's a me",
-        'title': "Blog Post 3",
-        'content': 'Third post content',
-        'date_posted': 'March 32, 2022'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request=request, template_name='blog/home.html', context=context)
 
