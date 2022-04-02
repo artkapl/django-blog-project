b43d7d0aefe8ab9189028c2506556fafb1a2aa68
Arthur <arthurkaplan@gmx.at>
2022-04-02 18:59:47 +0200

    views.py: added comment listing message options

--- users/views.py
+++ users/views.py
@@ -4,3 +4,12 @@ from django.contrib.auth.forms import UserCreationForm
 def register(request):
     form = UserCreationForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+# messages options:
+    # messages.debug
+    # messages.info
+    # messages.warning
+    # messages.success
+    # messages.error
+
