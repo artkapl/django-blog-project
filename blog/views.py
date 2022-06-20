57a1298e6b152313fbd241b9f44d69901deb6891
Arthur <arthurkaplan@gmx.at>
2022-06-20 19:27:27 +0200

    Added class for post ListView and DetailView

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 
@@ -7,6 +8,22 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request=request, template_name='blog/home.html', context=context)
+
+
+# Use class view instead of home() function view
+class PostListView(ListView):
+    model = Post
+    # By default, Django expects template to be in:
+    # <app>/<model>_<viewtype>.html
+    # change to existing template:
+    template_name = 'blog/home.html'
+
+    context_object_name = 'posts'
+    ordering = ['-date_posted']  # sort by newest to oldest
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 def about(request):
